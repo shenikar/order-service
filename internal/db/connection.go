ff02f8a277e00a6969de5af1eecfb71ef3549990
shenikar <ishmatov_i@mail.ru>
2025-08-06 21:53:25 +0300

    added connection db

---
+++ internal/db/connection.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"fmt"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+	"github.com/shenikar/order-service/config"
+)
+
+var DB *sqlx.DB
+
+// Connect устанавливает соединение с базой данных
+func Connect(config *config.Config) error {
+	connStr := config.GetDatabaseUrl()
+
+	var err error
+	DB, err = sqlx.Connect("pgx", connStr)
+	if err != nil {
+		return fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	// Проверка соединения
+	if err = DB.Ping(); err != nil {
+		return fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	fmt.Println("Connected to the database successfully")
+	return nil
+}
