cde9e72323acca7af85ec32a823ddededb06b800
shenikar <ishmatov_i@mail.ru>
2025-08-12 12:47:46 +0300

    added router

---
+++ internal/router/router.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/shenikar/order-service/internal/handler"
+)
+
+func SetupRoutes(engine *gin.Engine, orderHandler *handler.OrderHandler) {
+	engine.LoadHTMLFiles("web/index.html")
+
+	// Основные маршруты
+	engine.GET("/", orderHandler.Index)
+	engine.GET("/orders/:order_uid", orderHandler.GetOrder)
+}
