ba65387a07d701d0862a8b81cf90c41e58a71874
shenikar <ishmatov_i@mail.ru>
2025-08-12 12:40:08 +0300

    added handler layer

---
+++ internal/handler/order_handler.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/shenikar/order-service/internal/service"
+)
+
+type OrderHandler struct {
+	orderService *service.OrderService
+}
+
+// NewOrderHandler создает новый экземпляр OrderHandler
+func NewOrderHandler(orderService *service.OrderService) *OrderHandler {
+	return &OrderHandler{
+		orderService: orderService,
+	}
+}
+
+// GetOrder получает заказ по OrderUID
+func (h *OrderHandler) GetOrder(c *gin.Context) {
+	orderUID := c.Param("order_uid")
+
+	order, err := h.orderService.GetOrderByUID(orderUID)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Order not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, order)
+}
+
+// Index отображает главную страницу
+func (h *OrderHandler) Index(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", nil)
+}
