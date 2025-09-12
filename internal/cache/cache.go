999e55ffdcf14cd1a40b12623641499e5dee3ec7
shenikar <shenikar@student.21-school.ru>
2025-09-12 22:52:26 +0300

    added lru for cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,34 +1,28 @@
 package cache
 
 import (
-	"sync"
+	"time"
 
+	"github.com/hashicorp/golang-lru/v2/expirable"
 	"github.com/shenikar/order-service/internal/models"
 )
 
 type Cache struct {
-	data map[string]models.Order
-	mu   sync.RWMutex
+	lru *expirable.LRU[string, models.Order]
 }
 
 // NewCache создает новый экземпляр кэша
-func NewCache() *Cache {
-	return &Cache{
-		data: make(map[string]models.Order),
-	}
+func NewCache(capacity int, ttl time.Duration) (*Cache, error) {
+	lru := expirable.NewLRU[string, models.Order](capacity, nil, ttl)
+	return &Cache{lru: lru}, nil
 }
 
 // Set добавляет или обновляет заказ в кэше
 func (c *Cache) Set(order models.Order) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.data[order.OrderUID] = order
+	c.lru.Add(order.OrderUID, order)
 }
 
 // Get извлекает заказ из кэша по OrderUID
 func (c *Cache) Get(orderUID string) (models.Order, bool) {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	order, ok := c.data[orderUID]
-	return order, ok
+	return c.lru.Get(orderUID)
 }
